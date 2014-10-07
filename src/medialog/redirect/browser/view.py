dc44041164b202ac6aee6a464bf75638daaa686e
Espen Moe-Nilssen <espen@medialog.no>
2014-10-08 00:35:05 +0200

    reverting to hard coding index name

--- src/medialog/redirect/browser/view.py
+++ src/medialog/redirect/browser/view.py
@@ -6,13 +6,13 @@ from Products.Five import BrowserView
 
 class RedirectView(BrowserView):
      	
-    def __call__(self, index='', index_value=''):
+    def __call__(self, index='kortnummer', index_value=''):
     	"""redirect to indexed content"""
     	    	
     	catalog = api.portal.get_tool(name='portal_catalog')
     	#content_items = catalog(index=index_value) 
     	#content_items = catalog(kortnummer=index_value) 
-    	content_items = catalog({index}=index_value) 
+    	content_items = catalog(kortnummer = index_value) 
     	content_url = content_items[0].getURL()
     	return self.context.REQUEST.RESPONSE.redirect(content_url)
     	

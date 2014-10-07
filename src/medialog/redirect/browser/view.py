d8e10218b72db5e55825fe26af9a4f1881bae439
Espen Moe-Nilssen <espen@medialog.no>
2014-10-07 16:41:34 +0200

    removed interface until it works

--- src/medialog/redirect/browser/view.py
+++ src/medialog/redirect/browser/view.py
@@ -6,10 +6,11 @@ from Products.Five import BrowserView
 
 class RedirectView(BrowserView):
      
-    def call(self):
+    def call(self, index='', index_value=''):
     	"""redirect to indexed content"""
     	    	
     	catalog = api.portal.get_tool(name='portal_catalog')
     	content_items = catalog(portal_type=index_value)
-    	return content_items[0]
-    
+    	#return content_items[0]
+    	import pdb; pdb.set_trace()
+    	return "ost"

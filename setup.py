3c7ccaeef4e887ba08f941683c96da4d947ce90c
Espen Moe-Nilssen <espen@medialog.no>
2014-12-12 14:59:41 +0100

    Fixed unicode errors

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 from setuptools import setup, find_packages
 
 
-version = '0.1'
+version = '0.2.1'
 
 
 setup(name='medialog.redirect',
@@ -21,16 +21,10 @@ setup(name='medialog.redirect',
         "Framework :: Plone :: 4.1",
         "Framework :: Plone :: 4.2",
         "Framework :: Plone :: 4.3",
-        "Framework :: Zope2",
-        "Framework :: Zope3",
         "Intended Audience :: Developers",
         "Intended Audience :: End Users/Desktop",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Topic :: Internet",
-        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='redirect plone diazo',
       author='Espen Moe-Nilssen',
--- src/medialog/redirect/browser/view.py
+++ src/medialog/redirect/browser/view.py
@@ -4,13 +4,16 @@
 from plone import api
 from Products.Five import BrowserView
 
+import urllib 
+
 class RedirectView(BrowserView):
         
     def __call__(self, index='kortnummer', index_value=''):
         """redirect to indexed content"""
                 
         catalog = api.portal.get_tool(name='portal_catalog')
-        query = {index: index_value} 
+
+        query = {index: index_value.decode('latin-1')} 
         content_items = catalog(**query) 
 
         if len(content_items) == 1:
@@ -18,5 +21,5 @@ class RedirectView(BrowserView):
             return self.context.REQUEST.RESPONSE.redirect(content_url)
         
         #nothing, or too much found... search for it
-        search_url = '/@@search?' +index + '=' + index_value
+        search_url = '/@@search?' +index + '=' + index_value.decode('latin-1')
         return self.context.REQUEST.RESPONSE.redirect(search_url)
